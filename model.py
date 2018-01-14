a06717ef5eb6a06c057d0be8671fa767b3e3afe2
Wenfeng <Irlyue@outlook.com>
2018-01-14 17:12:13 +0800

    Full model

--- model.py
+++ model.py
@@ -5,17 +5,41 @@ import tensorflow.contrib.slim as slim
 from collections import OrderedDict
 
 
-def model_placeholders(batch_size, input_size):
-    height, width = input_size
-    images = tf.placeholder(tf.uint8, shape=(batch_size, height, width, 3), name='images')
-    labels = tf.placeholder(tf.int32, shape=(batch_size, height, width), name='labels')
-    return images, labels
+def model_placeholders(batch_size):
+    images_local = tf.placeholder(tf.uint8, shape=(batch_size, 31, 31, 3), name='images_local')
+    images_global = tf.placeholder(tf.uint8, shape=(batch_size, 201, 201, 3), name='images_global')
+    labels = tf.placeholder(tf.int32, shape=(batch_size, 31, 31), name='labels')
+    return images_local, images_global, labels
 
 
 class PatchModel:
-    def __init__(self, images):
+    def __init__(self, images_local, images_global):
         with tf.name_scope('patch_model'):
+            local_pool2 = self._one_wind(images_local, 'local')
+            global_pool2 = self._one_wind(images_global, 'global')
+            pool2 = tf.concat([local_pool2, global_pool2], axis=3, name='pool2')
+            pool2_flatten = slim.flatten(pool2)
+            fc3 = slim.fully_connected(pool2_flatten,
+                                       num_outputs=500)
+            fc4 = slim.fully_connected(fc3,
+                                       num_outputs=2,
+                                       activation_fn=None)
+            prob = tf.nn.sigmoid(fc4, name='probability')
+
+        endpoints = OrderedDict()
+        endpoints['images_local'] = images_local
+        endpoints['images_global'] = images_global
+        endpoints['pool2'] = pool2
+        endpoints['fc3'] = fc3
+        endpoints['fc4'] = fc4
+        endpoints['prob'] = prob
+        self.endpoints = endpoints
+
+    def _one_wind(self, images, type_):
+        with tf.variable_scope(type_):
             images = tf.cast(images, tf.float32)
+            if type_ == 'global':
+                images = tf.image.resize_images(images, size=(31, 31))
             conv1 = slim.conv2d(images,
                                 num_outputs=60,
                                 kernel_size=(6, 6),
@@ -34,33 +58,16 @@ class PatchModel:
                                     kernel_size=(3, 3),
                                     stride=3,
                                     scope='pool2')
-            pool2_flatten = slim.flatten(pool2)
-            fc3 = slim.fully_connected(pool2_flatten,
-                                       num_outputs=500)
-            fc4 = slim.fully_connected(fc3,
-                                       num_outputs=2,
-                                       activation_fn=None)
-            prob = tf.nn.sigmoid(fc4, name='probability')
-
-        endpoints = OrderedDict()
-        endpoints['images'] = images
-        endpoints['conv1'] = conv1
-        endpoints['pool1'] = pool1
-        endpoints['conv2'] = conv2
-        endpoints['pool2'] = pool2
-        endpoints['fc3'] = fc3
-        endpoints['fc4'] = fc4
-        endpoints['prob'] = prob
-        self.endpoints = endpoints
+        return pool2
 
     def __repr__(self):
         myself = ''
-        myself += '\n'.join('{:<2} {:<10} {!r}{!r}'.format(i, key, op.dtype, op.shape.as_list())
+        myself += '\n'.join('{:<2} {:<15} {!r}{!r}'.format(i, key, op.dtype, op.shape.as_list())
                             for i, (key, op) in enumerate(self.endpoints.items()))
         return myself
 
 
 if __name__ == '__main__':
-    images, _ = model_placeholders(32, input_size=(31, 31))
-    model = PatchModel(images)
+    a, b, c = model_placeholders(32)
+    model = PatchModel(a, b)
     print(model)
--- test_model.py
+++ test_model.py
@@ -16,56 +16,60 @@ parser.add_argument('--batch_size', type=int, default=32)
 
 
 class RunningModel:
-    def __init__(self, batch_size, input_size, gpu_count=0):
+    def __init__(self, batch_size, gpu_count=0):
         with tf.Graph().as_default() as g:
-            image_ph, label_ph = model.model_placeholders(batch_size, input_size)
+            image_local, image_global, _ = model.model_placeholders(batch_size)
             self.batch_size = batch_size
-            self.input_size = input_size
-            self.image_ph = image_ph
-            self.net = model.PatchModel(image_ph)
+            self.image_local = image_local
+            self.image_global = image_global
+            self.net = model.PatchModel(image_local, image_global)
+            logger.info('\n******************* Patch Model ***********************\n%r\n' % self.net)
             self.sess = tf.Session(graph=g, config=tf.ConfigProto(device_count={'GPU': gpu_count}))
             self.sess.run(tf.global_variables_initializer())
 
     def pre_run_n_step(self, n=10):
         logger.info('Pre-run %d steps to warm up the CPU...' % n)
         with my_utils.Timer() as timer:
-            height, width = self.input_size
             for _ in range(n):
-                image = np.random.randint(255, size=(self.batch_size, height, width, 3), dtype=np.uint8)
-                self.inference_prob(image)
+                image_l = np.random.randint(255, size=(self.batch_size, 31, 31, 3), dtype=np.uint8)
+                image_g = np.random.randint(255, size=(self.batch_size, 201, 201, 3), dtype=np.uint8)
+                self.inference_prob(image_l, image_g)
         logger.info('Done in %.4fs(%.4fsecs per run)' % (timer.eclipsed, timer.eclipsed / n))
 
     def run_n_step(self, n_patches=100, same_image=True):
         n_runs = n_patches // self.batch_size
         logger.info('Run %d steps for %d patches...' % (n_runs, n_patches))
-        height, width = self.input_size
-        image = np.random.randint(255, size=(self.batch_size, height, width, 3), dtype=np.uint8)
+        image_l = np.random.randint(255, size=(self.batch_size, 31, 31, 3), dtype=np.uint8)
+        image_g = np.random.randint(255, size=(self.batch_size, 201, 201, 3), dtype=np.uint8)
         with my_utils.Timer() as timer:
             for _ in range(n_runs):
                 if not same_image:
-                    image = np.random.randint(255, size=(self.batch_size, height, width, 3), dtype=np.uint8)
-                self.inference_prob(image)
+                    image_l = np.random.randint(255, size=(self.batch_size, 31, 31, 3), dtype=np.uint8)
+                    image_g = np.random.randint(255, size=(self.batch_size, 201, 201, 3), dtype=np.uint8)
+                self.inference_prob(image_l, image_g)
         logger.info('Done in %.4fs(%.4fsecs per run)' % (timer.eclipsed, timer.eclipsed / n_runs))
 
-    def _build_feed_dict(self, image):
-        return {self.image_ph: image}
+    def _build_feed_dict(self, image_l, image_g):
+        return {self.image_local: image_l, self.image_global: image_g}
 
-    def inference(self, image, ops):
+    def inference(self, image_l, image_g, ops):
         if type(ops[0]) == str:
             ops = [self.net.endpoints[op] for op in ops]
-        return self.sess.run(ops, feed_dict=self._build_feed_dict(image))
+        return self.sess.run(ops, feed_dict=self._build_feed_dict(image_l, image_g))
 
-    def inference_prob(self, image):
-        return self.inference(image, ['prob'])[0]
+    def inference_prob(self, image_l, image_g):
+        return self.inference(image_l, image_g, ['prob'])[0]
 
 
 def test_forward_time(gpu_count=0):
-    input_size = 31, 31
-    mm = RunningModel(FLAGS.batch_size, input_size, gpu_count=gpu_count)
+    mm = RunningModel(FLAGS.batch_size, gpu_count=gpu_count)
     mm.pre_run_n_step()
     mm.run_n_step(n_patches=FLAGS.n_patches)
 
 
 if __name__ == '__main__':
     FLAGS = parser.parse_args()
+    logger.info('************************************************************************')
+    logger.info(FLAGS)
+    logger.info('************************************************************************')
     test_forward_time(FLAGS.n_gpus)
